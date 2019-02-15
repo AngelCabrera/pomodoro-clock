289c18df1adaf1fd36f69066e8af11a06b3481c8
Angel Cabrera <soyangelcabrera@gmail.com>
2019-02-15 11:13:21 -0400

    Updates

---
+++ precache-manifest.8a1c9c7c21f9e0d8e39ac19e94e244b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c417dc263937fad15c82",
+    "url": "/pomodoro-clock/static/js/runtime~main.c417dc26.js"
+  },
+  {
+    "revision": "fb630e9d5a5986273409",
+    "url": "/pomodoro-clock/static/js/main.fb630e9d.chunk.js"
+  },
+  {
+    "revision": "cd86a7f7a83fdc2c4e38",
+    "url": "/pomodoro-clock/static/js/2.cd86a7f7.chunk.js"
+  },
+  {
+    "revision": "fb630e9d5a5986273409",
+    "url": "/pomodoro-clock/static/css/main.1934c169.chunk.css"
+  },
+  {
+    "revision": "5e4dda6dbdcc979d02b83e090c90f66b",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
